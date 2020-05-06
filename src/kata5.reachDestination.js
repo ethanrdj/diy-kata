ad59f770b04a280dc53e1366dd34512cd40fbde1
ethanrdj <ethanrdavis96@gmail.com>
2020-05-06 14:26:17 +0100

    completed reachDestination  katas

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const howLong = distance / speed;
+    const roundUp = Math.ceil(howLong / 0.5) * 0.5;
+    return `I should be there in ${roundUp} hours.`
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+    expect(reachDestination(100, 18)).toEqual("I should be there in 6 hours.");
+    expect(reachDestination(220, 65)).toEqual("I should be there in 3.5 hours.");
+    expect(reachDestination(555, 80)).toEqual("I should be there in 7 hours.")
+  });
 });
