24cd0b58e379ca4b7a5120e106134fdc0b68a29f
ethanrdj <ethanrdavis96@gmail.com>
2020-05-06 13:43:26 +0100

    completed humancatdogyears  katas

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,26 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+const x = [];
+x[0] = number;
+
+if (number === 1) {
+    x[1] = 15
+}
+if ( number === 2) {
+    x[1] =24
+}
+if (number === 1) {
+    x[2] = 15
+}
+if ( number === 2) {
+    x[2] =24
+}
+if (number >= 3) {
+    x[1] = ((number - 2) *4) +24
+}
+if (number >= 3) {
+    x[2] = ((number - 2) *5) +24
+}
+return  x;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("returns the human, dog and cat year as an array when passed a number", () =>  {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+        expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+});
+});
